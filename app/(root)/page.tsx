955315c61d9da5470ee9274b839ee5d74c5b9992
Tumininu <ijitumininu@gmail.com>
2025-03-24 22:03:17 +0100

    add Header and Footer components; update RootLayout to include them

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,8 @@
-import { Button } from "@/components/ui/button";
-import Image from "next/image";
 
 export default function Home() {
   return (
     <>
-    <h1 className="h1-bold">Read</h1>
-    <Button>Click me</Button>
+      
     </>
   );
 }
