df6bf660593f21d274afa90bd034fb0c93b4cf29
Tumininu <ijitumininu@gmail.com>
2025-03-28 00:34:55 +0100

    remove commented header from layout; update logo image

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en">
         <body className={`${poppins.variable} antialiased`}>
-          <header className="flex justify-end items-center p-4 gap-4 h-16"></header>
+          {/* <header className="flex justify-end items-center p-4 gap-4 h-16"></header> */}
           {children}
         </body>
       </html>
